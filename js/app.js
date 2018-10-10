fccb833fa968a9cf215dacbcf2b974b468cbe44b
camgelozzi <cangelozzi@gmail.com>
2018-10-10 16:06:19 -0400

    base structure

---
+++ js/app.js
@@ -0,0 +1,44 @@
+// https://github.com/dianadi09/pureJSLightbox
+
+(() => {
+  console.log("Welcome to our Midterm.");
+
+  //! VARIABLES
+  const productTitle = document.querySelector('#title');
+
+  // toogle for nav hanburger menu
+  let toogle = document.querySelector(".navigation__checkbox");
+  toogle.checked = false;
+
+  //! FUNCTIONS
+  // add content to paragraphs with Javascript
+  function setCourseInfo(data) {
+    productTitle.textContent = data.product_name;
+
+  }
+
+  function getData() {
+    fetch('../../Blue_E_Angelozzi_C_Atkinson_J_Boyden_D_J_Robertson_J_Hackathon/midterm/config/database.php')
+      .then(res => res.json())
+      .then(data => {
+        setCourseInfo(data[0]);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+
+  //! EVENTS
+
+  // events on load
+  window.addEventListener("load", () => {
+    // toogle navigation from hamburger to normal when matching media query
+    if (window.matchMedia("(min-width: 700px)").matches) {
+      toogle.checked = true;
+    }
+  });
+
+  getData();
+
+})();
